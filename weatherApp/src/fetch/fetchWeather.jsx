8086c4a81eb8149a50bfb21988218919a831f61f
Ibrahim Haji Abdi <ec21621@qmul.ac.uk>
2023-03-08 15:25:28 +0000

    test

---
+++ weatherApp/src/fetch/fetchWeather.jsx
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import { useState } from 'preact/hooks'
+
+const FetchWeather = () => {
+    console.log('hello')
+    const [temp, setTemp] = useState(null) 
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=02adac3e6a991d9f737fa48145b6c2ae';
+
+    const parser = (json) => {
+        const temp_c = json['main']['temp'];
+
+        setTemp(Math.round(temp_c))
+    }
+
+    $.ajax({
+        url: url,
+        success: parser,
+        error: (err) => {console.log('error message: ' + err)}
+    })
+
+
+    return (
+        <div>
+            <p>
+                Temp: {temp}
+            </p>
+        </div>
+    )
+}
+
+export default FetchWeather;
