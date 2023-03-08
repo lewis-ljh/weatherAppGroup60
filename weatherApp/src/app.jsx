76c356f9f837e8ad13c16016cb62943361499d58
Sharusan17 <shariharan17@icloud.com>
2023-03-08 16:47:09 +0000

    main

--- weatherApp/src/app.jsx
+++ weatherApp/src/app.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import './app.css'
+import FetchWeather from './fetch/fetchWeather'
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -17,9 +18,13 @@ export function App() {
       </div>
       <h1>Weather App</h1>
       <div class="card">
+        <div>
+          <input ></input>
+        </div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <FetchWeather />
         <p>
           Edit <code>src/app.jsx</code> and save to test HMR
         </p>
